9a9b9bd0b0ce46a65623d1951dcf91daadbf0892
pengtaoa <1564328778@qq.com>
2020-05-08 00:06:13 +0800

    update

---
+++ 一些题目/obj.js
@@ -0,0 +1,32 @@
+var a = { b: 3 }
+
+function foo(obj) {
+    obj.b = 5
+
+    return obj
+}
+
+var aa = foo(a)
+
+console.log(a.b)
+
+console.log(aa.b)
+
+function Ofo() {}
+
+function Bick() {
+    this.name = 'mybick'
+}
+
+var myBick = new Ofo()
+
+Ofo.prototype = new Bick()
+
+var youbick = new Bick()
+
+console.log(myBick.name)
+
+console.log(youbick.name)
+let x = 'lisi';
+x = 122
+console.log(x);
--- 一些题目/奇怪的题目.js
+++ 一些题目/奇怪的题目.js
@@ -1,7 +1,6 @@
 const numbers = [1, 2, 3, 4, 5];
-const [y] = numbers;
-
-// console.log(y);
+const [y, x] = numbers;
+console.log(y, x);
 
 const name = "Lydia Hallie"
 console.log(name.padStart(15, '233'))
