1943692ce38b76252113ae063e651c39d466f3cc
pengtaoa <1564328778@qq.com>
2020-05-24 23:23:30 +0800

    review

--- 写写题目/flat.js
+++ 写写题目/flat.js
@@ -1,2 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-23 23:31:33
+ * @LastEditTime: 2020-05-24 23:23:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \复习计划\写写题目\flat.js
+ */
 let arr = [1, 2, 4, [4, 2, 1, [5, 6, 7, [3, 5]]]]
-console.log(arr.flat(3));
+console.log(arr.flat(3))
