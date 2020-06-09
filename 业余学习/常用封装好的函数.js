45f315b6b963ad9f35a457a61b019a08fb1aa07e
pengtaoa <1564328778@qq.com>
2020-06-09 19:52:17 +0800

    update

--- 业余学习/常用封装好的函数.js
+++ 业余学习/常用封装好的函数.js
@@ -1,5 +1,6 @@
 const getQueryString = (name) => {
     const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    // const search = window.location.search.split('?')[1] || '';
     const search = window.location.search.split('?')[1] || '';
     const r = search.match(reg) || [];
     return r[2];
