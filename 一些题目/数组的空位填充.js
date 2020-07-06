eb0704e3f9529cc9407daa7c2db47b3737aa6be9
pengtaoa <1564328778@qq.com>
2020-07-06 19:57:04 +0800

    update

--- 一些题目/数组的空位填充.js
+++ 一些题目/数组的空位填充.js
@@ -1,8 +1,16 @@
 //数组的空位填充
-// 注意，你要原地修改原先的数组，而不是返回一个新的数组。
-const fillEmpty = (arr) => {
-    console.log(arr);
+// // 注意，你要原地修改原先的数组，而不是返回一个新的数组。
+// const fillEmpty = (arr) => {
+//     console.log(arr, "---", Array.from(arr));
 
-    return Array.from(arr).map((item, inx) => (inx in arr ? item : "hello"));
+//     return Array.from(arr).map((item, inx) => (inx in arr ? item : "hello"));
+// };
+const fillEmpty = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(i in arr);
+
+        if (!(i in arr)) arr[i] = "hello";
+    }
+    return arr;
 };
-console.log(fillEmpty([, 2, 3, , , , ,]));
+console.log(fillEmpty([, 2, 3, , undefined, , ,]));
