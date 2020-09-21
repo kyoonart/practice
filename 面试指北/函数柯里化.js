0cd75446007600c07431b5cf374d3fc053fe0b20
pengtaoa <1564328778@qq.com>
2020-09-21 22:34:43 +0800

    函数科里化

--- 面试指北/函数柯里化.js
+++ 面试指北/函数柯里化.js
@@ -2,8 +2,8 @@ function sum(x) {
     return (y) => {
         return (z) => {
             return x + y + z;
-        }
-    }
+        };
+    };
 }
 // console.log(sum(1)(3)(1));
 
@@ -12,15 +12,15 @@ function add(x) {
     sum += x;
     return function temp(b) {
         if (arguments.length == 0) {
-            return sum
+            return sum;
         } else {
             sum += b;
-            return temp
+            return temp;
         }
-    }
+    };
 }
-let sumx = add(1)(2)(3)
-console.log(sumx());
+let sumx = add(1)(2)(3);
+// console.log(sumx());
 
 // function curry(func) {
 //     var args = Array.prototype.slice.call(arguments, 1)
@@ -35,3 +35,28 @@ console.log(sumx());
 // }
 // let sum = curry(add, 1)
 // console.log(sum(8));
+
+// 无限累加函数
+
+function add(a) {
+    function sum(b) {
+        a = b ? a + b ? a;
+        return sum;
+    }
+    sum.toString = function() {
+        return a
+    }
+    return sum;
+}
+// 科里化实现多参数
+function addx() {
+    // 1:取出所有的参数
+    let args = [...arguments];
+    let fn = function() {
+        return addx.apply(null, args.concat([...arguments]));
+    };
+    fn.toString = () => args.reduce((pre, cur) => pre + cur);
+    return fn;
+}
+let a = addx(1)(2, 4)(3, 5);
+console.log(a);
