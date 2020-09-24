8ece120a26f29cec9e7cfdf4826c5278fb787fc6
pengtaoa <1564328778@qq.com>
2020-09-24 21:46:06 +0800

    feat:change

--- 每天一道/排序/睡眠排序.js
+++ 每天一道/排序/睡眠排序.js
@@ -1,13 +1,15 @@
 var sleepSort = function(arr, callback) {
     let res = [];
-    arr.forEach(item => {
+    arr.forEach((item) => {
         setTimeout(() => {
-            res.push(item)
+            res.push(item);
             if (res.length === arr.length) callback(res);
-        }, item)
+        }, item);
     });
-}
+};
 let arr = [1, 8, 2, 6, 2, 3, 22, 63, 1000];
-console.time('A')
-sleepSort(arr, res => { console.log(res) });
-console.timeEnd('A')
+console.time("A");
+sleepSort(arr, (res) => {
+    console.log(res);
+});
+console.timeEnd("A");
