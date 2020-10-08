ef39bc61775abb25c405c18ac84ae486070a715a
pengtaoa <1564328778@qq.com>
2020-10-08 22:34:32 +0800

    10/8

---
+++ 每天一道/数组/数组中重复的数组.js
@@ -0,0 +1,42 @@
+// var findRepeatNumber = function(nums) {
+//     let res = {}
+//     for (let i = 0; i < nums.length; i++) {
+//         if (!(nums[i] in res)) {
+//             res[nums[i]] = 1;
+//         } else {
+//             res[nums[i]]++;
+//         }
+//     }
+//     for (const key in res) {
+//         if (res.hasOwnProperty(key)) {
+//             if (res[key] > 1) {
+//                 return key;
+//             }
+
+//         }
+//     }
+// };
+// findRepeatNumber([2, 3, 1, 0, 2, 5, 3])
+//     // 或者
+// var findRepeatNumber = function(nums) {
+//     let s = new Set();
+//     for (var i in nums) {
+//         var curLenth = s.size;
+//         s.add(nums[i]);
+//         if (s.size == curLenth)
+//             return nums[i];
+//     }
+// };
+var findRepeatNumber = function(nums) {
+    const map = {};
+    for (const num of nums) {
+        console.log(map);
+
+        if (!map[num]) {
+            map[num] = true;
+        } else {
+            return num;
+        }
+    }
+};
+findRepeatNumber([2, 3, 1, 0, 2, 5, 3])
