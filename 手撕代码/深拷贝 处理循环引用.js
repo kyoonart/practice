ef6e093c868f3be73df43a8893c509be43389db9
pengtaoa <1564328778@qq.com>
2021-05-26 17:31:06 +0800

    5/26

---
+++ 手撕代码/深拷贝 处理循环引用.js
@@ -0,0 +1,38 @@
+function getType(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
+
+function deepClone(source, hash = new Map()) {
+  if (getType(source) !== Object && source !== null) {
+    return source;
+  }
+  if (hash.get(source)) return hash.get(source);
+  let target = Array.isArray(source) ? [] : {};
+  hash.set(source, target);
+  for (const key in source) {
+    if (getType(source) === "Object") {
+      deepClone(source[key], hash);
+    } else {
+      target[key] = source[key];
+    }
+  }
+  return target;
+}
+// 解决循环引用 map 或者 数组 把对象值存下来 即可
+var a = {
+  name: "muyiy",
+  book: {
+    title: "You Don't Know JS",
+    price: "45",
+  },
+  a1: undefined,
+  a2: null,
+  a3: 123,
+};
+var b = deepClone(a);
+
+a.name = "高级前端进阶";
+a.book.price = "55";
+// a.circleRef = a;
+console.log(b);
+console.log(JSON.parse(JSON.stringify(a)));
--- 每天一道/字符串/替换空格.js
+++ 每天一道/字符串/替换空格.js
@@ -1,4 +1,6 @@
-var replaceSpace = function(s) {
-    let temp = s.split(' ');
-    return temp.map((item, index) => index === temp.length - 1 ? item : `${item}%20`).join('');
+var replaceSpace = function (s) {
+  let temp = s.split(" ");
+  return temp
+    .map((item, index) => (index === temp.length - 1 ? item : `${item}%20`))
+    .join("");
 };
