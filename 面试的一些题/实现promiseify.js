e8587cae734eb7c810ca981afc81e63ab3524f85
pengtaoa <1564328778@qq.com>
2021-06-05 17:47:22 +0800

    格式化失效

--- 面试的一些题/实现promiseify.js
+++ 面试的一些题/实现promiseify.js
@@ -1,25 +1,22 @@
-
-const promisify = (fn)=> {
-}
-eg:
-ws.readFile = (url, callback) => {}
-ws.readFile('./abc.jpg', (err,data)=>{
-console.log(data);
-})
+const promisify = (fn) => {};
+eg: ws.readFile = (url, callback) => {};
+ws.readFile("./abc.jpg", (err, data) => {
+  console.log(data);
+});
 const readFilePromisise = promisify(ws.readFile);
-(async ()=>{
-const data = await readFilePromisise('/etc/xxx.jpg')
-console.log(data);
-})()
+(async () => {
+  const data = await readFilePromisise("/etc/xxx.jpg");
+  console.log(data);
+})();
 //
-const promisify = (fn)=> {
-    return  (...args) => {
-          return new Promise((resolve,reject)=>{
-              args.push((err,data)=>{
-                  if(err) reject(err);
-                  else resolve(data);
-              })
-            fn.apply(null,args); // 真正执行函数的地方
-          })
-      }
- }
+const promisify = (fn) => {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      args.push((err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+      fn.apply(null, args); // 真正执行函数的地方
+    });
+  };
+};
