c542d51a0ff7439ffe5120cd8dc362b4d9dd4e0a
pengtaoa <1564328778@qq.com>
2021-06-30 23:59:41 +0800

    :fire: 6/30

--- 前端数据处理的练习题目/id路径.js
+++ 前端数据处理的练习题目/id路径.js
@@ -1,41 +1,51 @@
 // 有这么一个数据结构:
 
-const data = [{
-        id: "1",
-        sub: [{
-            id: "2",
-            sub: [{
-                    id: "3",
-                    sub: null,
-                },
-                {
-                    id: "4",
-                    sub: [{
-                        id: "6",
-                        sub: null,
-                    }, ],
-                },
-                {
-                    id: "5",
-                    sub: null,
-                },
-            ],
-        }, ],
-    },
-    {
-        id: "7",
-        sub: [{
-            id: "8",
-            sub: [{
-                id: "9",
+const data = [
+  {
+    id: "1",
+    sub: [
+      {
+        id: "2",
+        sub: [
+          {
+            id: "3",
+            sub: null,
+          },
+          {
+            id: "4",
+            sub: [
+              {
+                id: "6",
                 sub: null,
-            }, ],
-        }, ],
-    },
-    {
-        id: "10",
-        sub: null,
-    },
+              },
+            ],
+          },
+          {
+            id: "5",
+            sub: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "7",
+    sub: [
+      {
+        id: "8",
+        sub: [
+          {
+            id: "9",
+            sub: null,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "10",
+    sub: null,
+  },
 ];
 // 现在给定一个id， 要求实现一个函数
 
@@ -49,18 +59,54 @@ const data = [{
 // id = "9" => ["7", "8", "9"]
 // id = "100" => []
 // PS: id 全局唯一， 无序
-const fn = (id, arr) => {
-    let result = [];
-    arr.forEach((it) => {
-        it.sub && it.sub.forEach((item) => {
-            if (item.id === id && item.sub === null) {
-                return result;
-            } else {
-                item.sub && result.push(item.id);
-                fn(item.id, item.sub);
-            }
-        });
-    })
+// function isContinued(arr) {
+//   let flag = false;
+//   let len = arr.length - 1;
+//   let newArr = arr.map((i) => +i);
+//   if ((newArr[len] - newArr[0]) / len === 1) return true;
+//   return false;
+// }
+// console.log(isContinued(["1", "2", "3", "4"]));
+
+// let result = [];
+// const fn = (id, arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     let temp = arr[i];
+//     if (result.id === id) {
+//       return result;
+//     } else {
+//       result.push(temp.id);
+//     }
+//   }
+// };
+var returnedItem = [];
+var find = function (arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id == id) {
+      return returnedItem;
+      break;
+    } else if (arr[i].sub && arr[i].sub.length > 0) {
+      returnedItem.push(arr[i].id);
+      find(arr[i].sub, id); //递归调用
+    }
+  }
+  return returnedItem;
+
+  // arr.forEach((item) => { //利用foreach循环遍历
+  //     if(item.id==id)//判断递归结束条件
+  //     {
+  //         returnedItem = item;
+  //         return item;
+  //     }
+  //     else if(item.children.length > 0) //判断chlidren是否有数据
+  //     {
+  //         find(item.children, id);  //递归调用
+  //     }
+  // })
 };
-let f = fn('7', data);
-console.log(f);
+
+// var item = find(array, 187);
+console.log(find(data, 6));
+
+// let f = findPath("7", data);
+// console.log(f);
