013d7195abab0bec6c79c7198e3534b11d46b3e4
pengtaoa <1564328778@qq.com>
2021-09-04 15:41:26 +0800

    9/3

---
+++ React/hook/capture value.js
@@ -0,0 +1,20 @@
+import React, { useState, useCallback } from 'react';
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  const handleAlertClick = useCallback(() => {
+    setTimeout(() => {
+      alert('You clicked on: ' + count);
+    }, 3000);
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>增加 count</button>
+      <button onClick={handleAlertClick}>显示 count</button>
+    </div>
+  );
+}
+export default Example;
