9960cb9662bbd251f96572ddf690e931bb4ddaf4
pengtaoa <1564328778@qq.com>
2021-09-10 10:52:13 +0800

    9/10

--- 常见题目/数组/旋转二维数组.js
+++ 常见题目/数组/旋转二维数组.js
@@ -3,7 +3,7 @@ let arr = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-const revortArr = (arr) => {
+const revortArr = arr => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i; j < arr[i].length; j++) {
       [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]];
@@ -15,4 +15,46 @@ const revortArr = (arr) => {
   return arr;
 };
 let res = revortArr(arr);
-console.log("res: ", res);
+console.log('res: ', res);
+// 螺旋矩阵
+var spiralOrder = function (matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const count = m * n;
+  const positions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  const ans = [];
+  let d = 0;
+  let row = 0,
+    col = -1;
+  while (ans.length < count) {
+    const rowNext = row + positions[d][0];
+    const colNext = col + positions[d][1];
+    console.log('rowNext', rowNext);
+    if (
+      rowNext === m ||
+      rowNext < 0 ||
+      colNext === n ||
+      colNext < 0 ||
+      matrix[rowNext][colNext] === ''
+    ) {
+      d = (d + 1) % positions.length;
+      continue;
+    }
+    ans.push(matrix[rowNext][colNext]);
+    matrix[rowNext][colNext] = '';
+    row = rowNext;
+    col = colNext;
+  }
+  console.log('xx', ans);
+  return ans;
+};
+spiralOrder([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
