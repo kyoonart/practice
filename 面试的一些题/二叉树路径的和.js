308e05d99f1800c024d1cd36c0c8fe60d50355ca
pengtaoa <1564328778@qq.com>
2021-09-16 13:15:05 +0800

    9/16

---
+++ 面试的一些题/二叉树路径的和.js
@@ -0,0 +1,25 @@
+// 给定一棵树，请你输出所有从根节点到叶子节点的路径组成的数字之和
+let tree = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: null,
+    right: null,
+  },
+};
+
+// 例如以上的树，总共有从根节点到叶子节点的路径3条，分别为：1->2->4,1->2->5,1->3
+// 则计算方法为：124+125+13=262
