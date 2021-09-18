b55211f9d6f660c635b8c276d2683dddc71d1db6
pengtaoa <1564328778@qq.com>
2021-09-18 17:51:07 +0800

    :zap: 9/18

--- 常见题目/二叉树/二叉树的层序遍历.js
+++ 常见题目/二叉树/二叉树的层序遍历.js
@@ -10,3 +10,42 @@ var levelOrder = function (root) {
   recursion(root, result, 0);
   return result;
 };
+
+// 变形   按照层级输出对象的属性key
+function recursion(node, result, index) {
+  if (!node) return null;
+  let keys = Object.keys(node);
+  let next = [];
+  keys.forEach((k) => {
+    next.push(node[k]);
+  });
+  if (keys.length) {
+    result[index] ? result[index].push(...keys) : (result[index] = [...keys]);
+  }
+  const newIndex = index + 1;
+
+  next.forEach((o) => {
+    recursion(o, result, newIndex);
+  });
+}
+var levelOrder = function (root) {
+  const result = [];
+  recursion(root, result, 0);
+  return result;
+};
+
+let obj = {
+  b: {
+    c: {
+      d: {
+        i: {},
+      },
+    },
+  },
+  f: {
+    g: {
+      l: {},
+    },
+  },
+};
+console.log(levelOrder(obj));
