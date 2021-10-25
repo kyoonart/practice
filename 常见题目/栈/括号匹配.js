9e854dece888bbeefb353fecdea466310e8970f2
pengtaoa <1564328778@qq.com>
2021-10-25 17:16:29 +0800

    :fire: 10/25

--- 常见题目/栈/括号匹配.js
+++ 常见题目/栈/括号匹配.js
@@ -1,23 +1,23 @@
-var isValid = function(s) {
-    let map = {
-        '(': -1,
-        ')': 1,
-        "{": -1,
-        '}': 1,
-        '[': -1,
-        ']': 1
+var isValid = function (s) {
+  let map = {
+    "(": -1,
+    ")": 1,
+    "{": -2,
+    "}": 2,
+    "[": -3,
+    "]": 3,
+  };
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < 0) {
+      stack.push(s[i]);
+    } else {
+      let last = stack.pop();
+      if (map[last] + map[s[i]] !== 0) return false;
     }
-    let stack = []
-    for (let i = 0; i < s.length; i++) {
-        if (map[s[i]] < 0) {
-            stack.push(s[i])
-        } else {
-            let last = stack.pop()
-            if (map[last] + map[s[i]] !== 0) return false;
-        }
-    }
-    if (stack.length > 0) return false
-    return true;
-}
-let res = isValid('{{{}}}')
+  }
+  if (stack.length > 0) return false;
+  return true;
+};
+let res = isValid("([)]");
 console.log(res);
